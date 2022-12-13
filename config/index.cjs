f8e2194f69d701e60a65a80812a7c99b436c1e8c
Deeru-Lu <63342998+Deeru-Lu@users.noreply.github.com>
2022-12-13 10:34:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -12,20 +12,25 @@ const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'bd63060d7756f1555660619e81df4ad3',
-
-  PROVINCE: '深圳',
-  CITY: '龙岗',
+  MAX_PUSH_ONE_MINUTE: 5,
+  // 配合MAX_PUSH_ONE_MINUTE使用，休眠<SLEEP_TIME>毫秒后再发送剩余消息，不填则默认为65000
+  SLEEP_TIME: 65000,
+  //PROVINCE: '深圳',
+  //CITY: '龙岗',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '主人',
+      name: '小卢',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ojzPe6rfh-Z0cDu56dyfvjR_Ibk4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'bINdjIqexK0K_Q27z-xPkf7jGLfAijkZDGe2xnBC4CM',
+      useTemplateId: 'MCx98a6oY3RJ2E9AVEMLXA71UQ0H2yg1sMm8iaCf2qw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-04',
+      
+  PROVINCE: '深圳',
+  CITY: '龙岗',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -61,28 +66,43 @@ const USER_CONFIG = {
     },
     {
        // 想要发送的人的名字
-      name: '主人',
+      name: '小卢',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ojzPe6rfh-Z0cDu56dyfvjR_Ibk4',
       //星座寄语发送模板
-      useTemplateId: '516iFxZTIrHIZqkkaD_SdlkvI4imyW2W1S9-ROQ1S8A',
+      useTemplateId: 'tPwxgVVHBUMbaQNJABB4D0b1pT22bAghvnP5AwCJvRE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-04',
     }, 
  
+   {
+     //每日一句
+       // 想要发送的人的名字
+      name: '小卢',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ojzPe6rfh-Z0cDu56dyfvjR_Ibk4',
+      //星座寄语发送模板
+      useTemplateId: 'SQVZVUfizlnb5dv6ND6lSvTfnAjGz-DqxSWJIPDbV9w',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '07-04',
+    }, 
+    
     {
       // 想要发送的人的名字
       name: '叉姐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ojzPe6szP3r6pYz0yjT_Xz3P5CdY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'bINdjIqexK0K_Q27z-xPkf7jGLfAijkZDGe2xnBC4CM',
+      useTemplateId: 'MCx98a6oY3RJ2E9AVEMLXA71UQ0H2yg1sMm8iaCf2qw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '11-04',
+      horoscopeDate: '04-18',
+      
+      PROVINCE: '深圳',
+      CITY: '龙岗',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '叉姐', year: '1999', date: '11-04',
+          type: '生日', name: '叉姐', year: '1999', date: '04-18',
         },
         {
           type: '*节日', name: '端午节', year: '1996', date: '05-05',
@@ -118,10 +138,154 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ojzPe6szP3r6pYz0yjT_Xz3P5CdY',
       //星座寄语发送模板
-      useTemplateId: '516iFxZTIrHIZqkkaD_SdlkvI4imyW2W1S9-ROQ1S8A',
+      useTemplateId: 'tPwxgVVHBUMbaQNJABB4D0b1pT22bAghvnP5AwCJvRE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '11-04',
-    },  
+      horoscopeDate: '04-18',
+    },
+      {
+     //每日一句
+       // 想要发送的人的名字
+      name: '叉姐',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ojzPe6szP3r6pYz0yjT_Xz3P5CdY',
+      //星座寄语发送模板
+      useTemplateId: 'SQVZVUfizlnb5dv6ND6lSvTfnAjGz-DqxSWJIPDbV9w',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '04-18',
+    },
+    
+    {
+      // 想要发送的人的名字
+      name: '陈芝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ojzPe6n7w2mLa8SdW6TxAZHo2LdI',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'MCx98a6oY3RJ2E9AVEMLXA71UQ0H2yg1sMm8iaCf2qw',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '03-27',
+      
+      PROVINCE: '深圳',
+      CITY: '龙华',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '陈芝', year: '1999', date: '02-10',
+        },
+        {
+          type: '*节日', name: '端午节', year: '1996', date: '05-05',
+        },
+        {
+          type: '*节日', name: '中秋节', year: '1996', date: '08-15',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '节日', name: '元旦', year: '2023', date: '01-01',
+        },
+        {
+         type: '节日', name: '清明节', year: '2023', date: '04-05',
+        },
+        {
+          type: '节日', name: '劳动节', year: '2022', date: '05-01',
+        },
+        {
+          type: '节日', name: '国庆节', year: '2022', date: '10-01',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+    {
+       // 想要发送的人的名字
+      name: '陈芝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ojzPe6n7w2mLa8SdW6TxAZHo2LdI',
+      //星座寄语发送模板
+      useTemplateId: 'tPwxgVVHBUMbaQNJABB4D0b1pT22bAghvnP5AwCJvRE',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '03-27',
+    },
+      {
+     //每日一句
+       // 想要发送的人的名字
+      name: '陈芝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ojzPe6n7w2mLa8SdW6TxAZHo2LdI',
+      //星座寄语发送模板
+      useTemplateId: 'SQVZVUfizlnb5dv6ND6lSvTfnAjGz-DqxSWJIPDbV9w',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '03-27',
+    },
+    {
+      // 想要发送的人的名字
+      name: '点哥',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ojzPe6rWvWiqjEc0biFILW8B4cqc',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'MCx98a6oY3RJ2E9AVEMLXA71UQ0H2yg1sMm8iaCf2qw',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '07-27',
+      
+      PROVINCE: '江苏',
+      CITY: '无锡',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '生日', name: '点哥', year: '1999', date: '07-27',
+        },
+        {
+          type: '*节日', name: '端午节', year: '1996', date: '05-05',
+        },
+        {
+          type: '*节日', name: '中秋节', year: '1996', date: '08-15',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '节日', name: '元旦', year: '2023', date: '01-01',
+        },
+        {
+         type: '节日', name: '清明节', year: '2023', date: '04-05',
+        },
+        {
+          type: '节日', name: '劳动节', year: '2022', date: '05-01',
+        },
+        {
+          type: '节日', name: '国庆节', year: '2022', date: '10-01',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+    {
+       // 想要发送的人的名字
+      name: '点哥',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ojzPe6rWvWiqjEc0biFILW8B4cqc',
+      //星座寄语发送模板
+      useTemplateId: 'tPwxgVVHBUMbaQNJABB4D0b1pT22bAghvnP5AwCJvRE',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '07-27',
+    },
+      {
+     //每日一句
+       // 想要发送的人的名字
+      name: '点哥',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ojzPe6rWvWiqjEc0biFILW8B4cqc',
+      //星座寄语发送模板
+      useTemplateId: 'SQVZVUfizlnb5dv6ND6lSvTfnAjGz-DqxSWJIPDbV9w',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '07-27',
+    }, 
   ],
 
   
